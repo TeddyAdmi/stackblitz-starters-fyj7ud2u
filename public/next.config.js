68d6cd8590df10c4f8b38bb6aa4913b21401da6f
TeddyAdmi <bitidiesel@gmail.com>
2026-09-10 00:19:32 +0300

    next.config.js

--- public/next.config.js
+++ public/next.config.js
@@ -1,17 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Разрешаем встраивание сайта в любые iframe и удаляем жесткие заголовки безопасности
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: '/(.*)',
         headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL',
-          },
           {
             key: 'Access-Control-Allow-Origin',
             value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: '*',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors * 'self' https://*.pocketnet.app https://*.bastyon.com https://*.bastyon.app;",
           },
         ],
       },
