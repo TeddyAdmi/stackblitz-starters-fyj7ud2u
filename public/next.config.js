c8e873b88da95821d7b9649a99c5e4054545c2e1
TeddyAdmi <bitidiesel@gmail.com>
2026-09-10 18:17:55 +0300

    next.config.js

--- public/next.config.js
+++ public/next.config.js
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Разрешаем встраивание сайта в любые iframe и удаляем жесткие заголовки безопасности
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
           {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
             key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
             value: '*',
           },
           {
